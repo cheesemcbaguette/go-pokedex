65c2817f366ca609c8f7d0f7b156af24560f5ebe
cheesemcbaguette <29225021+cheesemcbaguette@users.noreply.github.com>
2024-10-09 11:54:23 +0200

    Add explore functionnality

--- internal/pokeapi/type_locations.go
+++ internal/pokeapi/type_locations.go
@@ -10,3 +10,52 @@ type RespShallowLocations struct {
 		URL  string `json:"url"`
 	} `json:"results"`
 }
+
+type Location struct {
+	GameIndex            int64                 `json:"game_index"`
+	Names                []Name                `json:"names"`
+	PokemonEncounters    []PokemonEncounter    `json:"pokemon_encounters"`
+	Name                 string                `json:"name"`
+	Location             LocationClass         `json:"location"`
+	ID                   int64                 `json:"id"`
+	EncounterMethodRates []EncounterMethodRate `json:"encounter_method_rates"`
+}
+
+type EncounterMethodRate struct {
+	VersionDetails  []EncounterMethodRateVersionDetail `json:"version_details"`
+	EncounterMethod LocationClass                      `json:"encounter_method"`
+}
+
+type LocationClass struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type EncounterMethodRateVersionDetail struct {
+	Rate    int64         `json:"rate"`
+	Version LocationClass `json:"version"`
+}
+
+type Name struct {
+	Name     string        `json:"name"`
+	Language LocationClass `json:"language"`
+}
+
+type PokemonEncounter struct {
+	Pokemon        LocationClass                   `json:"pokemon"`
+	VersionDetails []PokemonEncounterVersionDetail `json:"version_details"`
+}
+
+type PokemonEncounterVersionDetail struct {
+	MaxChance        int64             `json:"max_chance"`
+	EncounterDetails []EncounterDetail `json:"encounter_details"`
+	Version          LocationClass     `json:"version"`
+}
+
+type EncounterDetail struct {
+	ConditionValues []interface{} `json:"condition_values"`
+	Chance          int64         `json:"chance"`
+	Method          LocationClass `json:"method"`
+	MaxLevel        int64         `json:"max_level"`
+	MinLevel        int64         `json:"min_level"`
+}
--- repl.go
+++ repl.go
@@ -26,10 +26,15 @@ func startRepl(cfg *config) {
 		}
 
 		commandName := words[0]
+		//store command arguments if there is more than one word typed
+		args := []string{}
+		if len(words) > 1 {
+			args = words[1:]
+		}
 
 		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback(cfg)
+			err := command.callback(cfg, args...)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -50,7 +55,7 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*config) error
+	callback    func(*config, ...string) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -74,6 +79,11 @@ func getCommands() map[string]cliCommand {
 			name:        "exit",
 			description: "Exit the Pokedex",
 			callback:    commandExit,
+		},
+		"explore": {
+			name:        "explore <location_name>",
+			description: "List all pokemons in a given area",
+			callback:    commandExplore,
 		},
 	}
 }
